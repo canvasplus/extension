99c0e061dd77d2bce9eb061157a9bc65cd13ce2e
Adrian <67245238+adrWasTaken@users.noreply.github.com>
2021-01-14 20:46:42 -0600

    Update index.html

--- scripts/universal.js
+++ scripts/universal.js
@@ -1,1 +1,8 @@
-alert("Hey!")
+window.bears = {}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  window.bears[request.url] = request.count
+})
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.create({url: '../index.html'})
+})
