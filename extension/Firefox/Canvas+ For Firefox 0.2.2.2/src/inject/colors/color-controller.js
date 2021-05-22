87510e4f141e5b3f9f8b340571ecd001792e7280
J Coding <83322224+J-Coding-Dev@users.noreply.github.com>
2021-05-22 09:12:37 -0500

    Delete extension/Firefox/Canvas+ For Firefox 0.2.2.2 directory

--- extension/Firefox/Canvas+ For Firefox 0.2.2.2/src/inject/colors/color-controller.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
-    var link = document.createElement("link");
-    link.href = chrome.extension.getURL("src/inject/colors/canvas-dark.css");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-      if(data["canvasplus-display-appearance"] == "dark") document.getElementsByTagName('html')[0].appendChild(link);
-});
