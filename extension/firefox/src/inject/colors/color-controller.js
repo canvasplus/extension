eb1ba33093f6b243ceb113da3cd26f0a5ca6df3b
Adrian <adrianfcasares@gmail.com>
2021-05-29 10:10:53 -0500

    Revert "Add Dim mode to Firefox 🦊🌓"
    This reverts commit 56cbd58e5e1bb5f30f7cde33458eb19d6d673c36.

--- extension/firefox/src/inject/colors/color-controller.js
+++ extension/firefox/src/inject/colors/color-controller.js
@@ -1,12 +1,7 @@
 chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
     var link = document.createElement("link");
-    link.href = chrome.extension.getURL("src/inject/colors/dark.css");
+    link.href = chrome.extension.getURL("src/inject/colors/canvas-dark.css");
     link.type = "text/css";
     link.rel = "stylesheet";
-    var link2 = document.createElement("link");
-    link2.href = chrome.extension.getURL("src/inject/colors/dim.css");
-    link2.type = "text/css";
-    link2.rel = "stylesheet";
       if(data["canvasplus-display-appearance"] == "dark") document.getElementsByTagName('html')[0].appendChild(link);
-      if(data["canvasplus-display-appearance"] == "dim") document.getElementsByTagName('html')[0].appendChild(link2);
 });
