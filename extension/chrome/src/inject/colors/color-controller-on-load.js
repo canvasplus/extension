be26742c26c0b4993ae8c7b619a0ef50dbfa3c5a
Adrian <adrianfcasares@gmail.com>
2021-05-29 10:10:45 -0500

    Revert "Add Dim Mode 🌓"
    This reverts commit 45280d29ce07e2c120143bd81c4b0615984d6fbc.

--- extension/chrome/src/inject/colors/color-controller-on-load.js
+++ extension/chrome/src/inject/colors/color-controller-on-load.js
@@ -5,27 +5,7 @@ chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
     link.type = "text/css";
     link.rel = "stylesheet";
     link.setAttribute("class", "cpt-iframe-style")
-    var link2 = document.createElement("link");
-    link2.href = chrome.extension.getURL("src/inject/colors/dim.css");
-    link2.type = "text/css";
-    link2.rel = "stylesheet";
-    link2.setAttribute("class", "cpt-iframe-style")
     if(data["canvasplus-display-appearance"] == "dark") document.addEventListener("scroll", function() {
-        if(document.getElementsByClassName("tox-edit-area__iframe").length > 0) {
-          for(let iframeWrapper of document.getElementsByClassName("tox-edit-area__iframe")) {
-            if(iframeWrapper.getAttribute("cpt-style-applied") == null) {
-            iframeWrapper.contentDocument.body.onclick = function() {
-                if(iframeWrapper.contentDocument.getElementsByTagName("head")[0].getElementsByClassName("cpt-iframe-style").length == 0) {
-                    iframeWrapper.contentDocument.getElementsByTagName("head")[0].appendChild(link);
-                }
-              }
-              iframeWrapper.setAttribute("cpt-style-applied", true)
-            }
-          }
-
-        }
-    });
-    if(data["canvasplus-display-appearance"] == "dim") document.addEventListener("scroll", function() {
         if(document.getElementsByClassName("tox-edit-area__iframe").length > 0) {
           for(let iframeWrapper of document.getElementsByClassName("tox-edit-area__iframe")) {
             if(iframeWrapper.getAttribute("cpt-style-applied") == null) {
--- extension/chrome/src/inject/colors/color-controller.js
+++ extension/chrome/src/inject/colors/color-controller.js
@@ -3,10 +3,5 @@ chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
     link.href = chrome.extension.getURL("src/inject/colors/dark.css");
     link.type = "text/css";
     link.rel = "stylesheet";
-    var link2 = document.createElement("link");
-    link2.href = chrome.extension.getURL("src/inject/colors/dim.css");
-    link2.type = "text/css";
-    link2.rel = "stylesheet";
       if(data["canvasplus-display-appearance"] == "dark") document.getElementsByTagName('html')[0].appendChild(link);
-      if(data["canvasplus-display-appearance"] == "dim") document.getElementsByTagName('html')[0].appendChild(link2);
 });
