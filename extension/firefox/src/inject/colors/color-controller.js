a94441299a98e0228eebe2e5b69fbd6d4c823e41
Adrian <adrianfcasares@gmail.com>
2021-05-31 17:13:35 -0500

    Separate variables and css rules for less repetition

--- extension/firefox/src/inject/colors/color-controller.js
+++ extension/firefox/src/inject/colors/color-controller.js
@@ -1,4 +1,8 @@
 chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
+  var linkApplyVars = document.createElement("link");
+  linkApplyVars.href = chrome.extension.getURL("src/inject/colors/apply-variables.css");
+  linkApplyVars.type = "text/css";
+  linkApplyVars.rel = "stylesheet";
   var link = document.createElement("link");
   link.href = chrome.extension.getURL("src/inject/colors/dark.css");
   link.type = "text/css";
@@ -7,6 +11,12 @@ chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
   link2.href = chrome.extension.getURL("src/inject/colors/dim.css");
   link2.type = "text/css";
   link2.rel = "stylesheet";
-    if(data["canvasplus-display-appearance"] == "dark") document.getElementsByTagName('html')[0].appendChild(link);
-    if(data["canvasplus-display-appearance"] == "dim") document.getElementsByTagName('html')[0].appendChild(link2);
+  if(data["canvasplus-display-appearance"] === "dark") {
+    document.getElementsByTagName('html')[0].appendChild(link);
+    document.getElementsByTagName('html')[0].appendChild(linkApplyVars);
+  }
+  if(data["canvasplus-display-appearance"] === "dim") {
+    document.getElementsByTagName('html')[0].appendChild(link2);
+    document.getElementsByTagName('html')[0].appendChild(linkApplyVars);
+  }
 });
