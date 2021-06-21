9d1fa0c6b689c82afebf1378d2ea7098b0faaa83
J Coding <jlozier99@gmail.com>
2021-06-21 13:25:44 -0500

    ✨Add Customizable Link Color

---
+++ extension/firefox/src/inject/textcolor/textcolor.js
@@ -0,0 +1,8 @@
+const otherplaceholder = "ffda16";
+const amount = document.getElementsByTagName("a");
+if (otherplaceholder != 0) {
+  for (i = 0; i in amount; i++) {
+    var a = document.getElementsByTagName("a")[i];
+    a.style.color = "#" + otherplaceholder;
+  };
+};
