19bdd2c92c1e43f334a1eaa66859c24cd7b5e307
J Coding <jlozier99@gmail.com>
2021-07-04 14:24:50 -0500

    🐛Fix Textcolor Missing Links

--- extension/firefox/src/inject/textcolor/textcolor.js
+++ extension/firefox/src/inject/textcolor/textcolor.js
@@ -1,8 +1,11 @@
-const otherplaceholder = "ffda16";
-const amount = document.getElementsByTagName("a");
-if (otherplaceholder != 0) {
-  for (i = 0; i in amount; i++) {
-    var a = document.getElementsByTagName("a")[i];
-    a.style.color = "#" + otherplaceholder;
+const otherplaceholder = "0";
+window.addEventListener('load', function() {
+  const amount = document.getElementsByTagName("a");
+  if (otherplaceholder != 0) {
+    for (i = 0; i in amount; i++) {
+      var a = document.getElementsByTagName("a")[i];
+      a.style.color = "#" + otherplaceholder;
+    };
   };
-};
+
+})
