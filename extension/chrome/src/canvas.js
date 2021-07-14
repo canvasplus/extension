91e6579bd80bde50b2ce030f3aae7a448aaa6544
J Coding <jlozier99@gmail.com>
2021-07-14 13:59:53 -0500

    🐛⚡Fix settings not being set + Add Background.js to Firefox

--- extension/chrome/src/canvas.js
+++ extension/chrome/src/canvas.js
@@ -1,11 +1,11 @@
-const settingsList = ["canvasplus-setting-quicklink", "canvasplus-setting-search", "canvasplus-setting-smartscroll", "canvasplus-display-appearance"];
+const settingsList = ["canvasplus-setting-quicklink", "canvasplus-setting-search", "canvasplus-setting-smartscroll", "canvasplus-display-appearance", "canvasplus-setting-navigator", "canvasplus-setting-convopeeker", "canvasplus-setting-sidebar-hidelogo", "canvasplus-setting-sidebar-color", "canvasplus-setting-sidebar-size-toggle", "canvasplus-setting-linkcolor-toggle"];
 let settings = {};
 
 chrome.storage.local.get(settingsList, function(data) {
   settings = data;
   if(Object.entries(data).length < settingsList.length)
   {
-    chrome.storage.local.set({"canvasplus-setting-quicklink": true, "canvasplus-setting-search": true, "canvasplus-setting-smartscroll": true, "canvasplus-display-appearance": "light"});
+    chrome.storage.local.set({"canvasplus-setting-quicklink": true, "canvasplus-setting-search": true, "canvasplus-setting-smartscroll": true, "canvasplus-display-appearance": "light", "canvasplus-setting-navigator": false, "canvasplus-setting-convopeeker": true, "canvasplus-setting-sidebar-hidelogo": false, "canvasplus-setting-sidebar-color": false, "canvasplus-setting-sidebar-size-toggle": false, "canvasplus-setting-linkcolor-toggle": false});
   }
 });
 
