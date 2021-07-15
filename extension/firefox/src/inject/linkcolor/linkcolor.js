d4c458ccc9d271060a0830e236c7bb7fac3e8e38
J Coding <jlozier99@gmail.com>
2021-07-15 15:31:47 -0500

    ⚡Improve Linkcolor

--- extension/firefox/src/inject/linkcolor/linkcolor.js
+++ extension/firefox/src/inject/linkcolor/linkcolor.js
@@ -2,10 +2,15 @@ chrome.storage.local.get(["canvasplus-setting-linkcolor-toggle"], function(toggl
   if(toggle["canvasplus-setting-linkcolor-toggle"]) {
     chrome.storage.local.get(["canvasplus-setting-linkcolor"], function(data) {
       const hexcode = data;
+      var amount = document.getElementsByTagName("a");
+      for (i = 0; i in amount; i++) {
+        var a = document.getElementsByTagName("a")[i];
+        a.style.color = "#" + hexcode;
+      };
       window.addEventListener('load', function() {
-        const amount = document.getElementsByTagName("a");
+        amount = document.getElementsByTagName("a");
         for (i = 0; i in amount; i++) {
-          var a = document.getElementsByTagName("a")[i];
+          a = document.getElementsByTagName("a")[i];
           a.style.color = "#" + hexcode;
         };
       });
