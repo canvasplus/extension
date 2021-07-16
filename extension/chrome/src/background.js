f2acd2153b5ce3cfd3333a81a0086de4ac813cda
J Coding <jlozier99@gmail.com>
2021-07-16 12:51:28 -0500

    ⚡Add default setting value when updated

--- extension/chrome/src/background.js
+++ extension/chrome/src/background.js
@@ -1,9 +1,10 @@
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
-        window.open("https://canvasplus.adrwas.dev/welcome");
-        chrome.storage.local.set({"canvasplus-setting-quicklink": true, "canvasplus-setting-search": true, "canvasplus-setting-smartscroll": true, "canvasplus-display-appearance": "light", "canvasplus-setting-navigator": false, "canvasplus-setting-convopeeker": true, "canvasplus-setting-sidebar-hidelogo": false, "canvasplus-setting-sidebar-color": false, "canvasplus-setting-sidebar-size-toggle": false, "canvasplus-setting-linkcolor-toggle": false});
+      window.open("https://canvasplus.adrwas.dev/welcome");
+      chrome.storage.local.set({"canvasplus-setting-quicklink": true, "canvasplus-setting-search": true, "canvasplus-setting-smartscroll": true, "canvasplus-display-appearance": "light", "canvasplus-setting-navigator": false, "canvasplus-setting-convopeeker": true, "canvasplus-setting-sidebar-hidelogo": false, "canvasplus-setting-sidebar-color": false, "canvasplus-setting-sidebar-size-toggle": false, "canvasplus-setting-linkcolor-toggle": false});
     }
-    // else if(details.reason == "update") {
+    else if (details.reason == "update") {
+      chrome.storage.local.set({"canvasplus-setting-navigator": false, "canvasplus-setting-convopeeker": true, "canvasplus-setting-sidebar-hidelogo": false, "canvasplus-setting-sidebar-color": false, "canvasplus-setting-sidebar-size-toggle": false, "canvasplus-setting-linkcolor-toggle": false});
     //   if(details.previousVersion != "0.2.2") window.open("https://canvasplus.adrwas.dev/update/patch-0-2-2");
-    // }
+    }
 });
