013faafe9e3feb532ff50b90a69ae255cc5d34a7
J Coding <jlozier99@gmail.com>
2021-08-16 19:36:14 -0500

    ⚡Add linkcolor color selector support

--- extension/firefox/src/inject/linkcolor/linkcolor.js
+++ extension/firefox/src/inject/linkcolor/linkcolor.js
@@ -1,19 +1,12 @@
-chrome.storage.local.get(["canvasplus-setting-linkcolor-toggle"], function(toggle) {
-  if(toggle["canvasplus-setting-linkcolor-toggle"]) {
-    chrome.storage.local.get(["canvasplus-setting-linkcolor"], function(data) {
-      const hexcode = data;
-      var amount = document.getElementsByTagName("a");
-      for (i = 0; i in amount; i++) {
-        var a = document.getElementsByTagName("a")[i];
-        a.style.color = "#" + hexcode;
-      };
-      window.addEventListener('load', function() {
-        amount = document.getElementsByTagName("a");
-        for (i = 0; i in amount; i++) {
-          a = document.getElementsByTagName("a")[i];
-          a.style.color = "#" + hexcode;
-        };
-      });
-    });
+chrome.storage.local.get(["canvasplus-setting-linkcolor"], function(data) {
+  const color = data["canvasplus-setting-linkcolor"];
+  console.log(color);
+  if(color.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/)) {
+    var link = document.createElement("link");
+    link.href = chrome.extension.getURL("src/inject/linkcolor/linkcolor.css");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName('html')[0].appendChild(link);
+    document.documentElement.style.setProperty('--ic-link-color', color);
   };
 });
