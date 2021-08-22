d9a97da6963286a3ceaf696f9e11ae939531e736
adrWasTaken <67245238+adrWasTaken@users.noreply.github.com>
2021-08-22 09:55:51 -0500

    Change icon size setting to smaller icons toggle

--- extension/chrome/popup/popup.js
+++ extension/chrome/popup/popup.js
@@ -89,9 +89,9 @@ const frames = {
         </div>
         <SettingGroup name="Sidebar">
           <Setting name="Hide Logo" setting="sidebar-hidelogo" description="Hide the logo on the top of the sidebar." />
-          <Setting name="Sidebar Background Color" setting="sidebar-color" description="Change the background color of the sidebar." defaultValue="#C00" customInput={(state, setState) => { return <ColorSwitch state={state} setState={setState} />}}/>
-          <Setting name="Sidebar Icon Color" setting="sidebar-icon-color" description="Change the icon color of the sidebar." defaultValue="unset" customInput={(state, setState) => { return <LimitedColorSwitch state={state} setState={setState} />}}/>
-          <Setting name="Sidebar Size" setting="sidebar-size-toggle" description="Change the size of the text and images on the sidebar." />
+          <Setting name="Background Color" setting="sidebar-color" description="Change the background color of the sidebar." defaultValue="#C00" customInput={(state, setState) => { return <ColorSwitch state={state} setState={setState} />}}/>
+          <Setting name="Icon Color" setting="sidebar-icon-color" description="Change the icon color of the sidebar." defaultValue="unset" customInput={(state, setState) => { return <LimitedColorSwitch state={state} setState={setState} />}}/>
+          <Setting name="Smaller Icons" setting="sidebar-smaller-icons" description="Decrease the size of sidebar icons." />
         </SettingGroup>
         <SettingGroup name="Other">
           <Setting name="Link Color" setting="linkcolor" description="Change the color of links on Canvas." defaultValue="#C00" customInput={(state, setState) => { return <ColorSwitch state={state} setState={setState} />}}/>
--- extension/chrome/src/inject/sidebar/sidebar.js
+++ extension/chrome/src/inject/sidebar/sidebar.js
@@ -1,26 +1,9 @@
-chrome.storage.local.get(["canvasplus-setting-sidebar-size-toggle"], function(toggle) {
-  if(toggle["canvasplus-setting-sidebar-size-toggle"]) {
-    chrome.storage.local.get(["canvasplus-setting-sidebar-size"], function(data) {
-        const sidebarwidth = data;
-        const text = document.getElementsByClassName("menu-item__text");
-        const icon = document.getElementsByClassName("ic-icon-svg");
-        for (i = 0; i < text.length; i++) {
-          text[i].style.fontSize = sidebarwidth + "px";
-        };
-        for (i = 0; i < icon.length; i++) {
-          icon[i].style.width = sidebarwidth * "1.85714285714" + "px";
-        };
-
-        chrome.storage.local.get(["canvasplus-setting-sidebar-hidelogo"], function(data) {
-          if(data["canvasplus-setting-sidebar-hidelogo"] != true) {
-            document.getElementsByClassName("ic-app-header__logomark")[0].style.height = sidebarwidth * "6.07142857143" + "px";
-          };
-        });
-        document.getElementsByClassName("fs-exclude ic-avatar")[0].style.height = sidebarwidth * "2.57142857143" + "px";
-        document.getElementsByClassName("fs-exclude ic-avatar")[0].style.width = sidebarwidth * "2.57142857143" + "px";
-        document.getElementsByClassName("menu-item-icon-container")[0].style.width = sidebarwidth * "2.57142857143" + "px";
-    });
-  };
+chrome.storage.local.get(["canvasplus-setting-sidebar-smaller-icons"], function(toggle) {
+  if(toggle["canvasplus-setting-sidebar-smaller-icons"]) {
+    for(let el of document.querySelectorAll(".menu-item-icon-container svg")) {
+      el.style = "width:22px";
+    }
+  }
 });
 
 chrome.storage.local.get(["canvasplus-setting-sidebar-hidelogo"], function(data) {
