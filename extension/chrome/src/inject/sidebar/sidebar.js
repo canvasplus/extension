652c700c9949eead9941c40362a54974307e56b2
adrWasTaken <67245238+adrWasTaken@users.noreply.github.com>
2021-08-22 20:42:37 -0500

    Add functioning active sidebar color changing

--- extension/chrome/src/inject/sidebar/sidebar.js
+++ extension/chrome/src/inject/sidebar/sidebar.js
@@ -22,7 +22,7 @@ var sidebarStyle = '';
 chrome.storage.local.get(["canvasplus-setting-sidebar-color"], function(data) {
   const color = data["canvasplus-setting-sidebar-color"];
   if(color.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/)) {
-    sidebarStyle += `--ic-brand-global-nav-bgd: ${ color };--ic-brand-global-nav-ic-icon-svg-fill--active: ${ color };--ic-brand-global-nav-avatar-border: ${ color };`;
+    sidebarStyle += `--ic-brand-global-nav-bgd: ${ color };--ic-brand-global-nav-avatar-border: ${ color };`;
     document.querySelector('#header').style = sidebarStyle;
   }
 });
@@ -31,6 +31,30 @@ chrome.storage.local.get(["canvasplus-setting-sidebar-icon-color"], function(dat
   const color = data["canvasplus-setting-sidebar-icon-color"];
   if(color !== "unset") {
     sidebarStyle += `--ic-brand-global-nav-ic-icon-svg-fill: ${ color };`;
+    document.querySelector('#header').style = sidebarStyle;
+  }
+});
+
+chrome.storage.local.get(["canvasplus-setting-active-sidebar-color"], (data) => {
+  const json = data["canvasplus-setting-active-sidebar-color"];
+  if(json) {
+    if(json.background === "blend") {
+      sidebarStyle += '--custom-active-background: var(--cpt-dark-background-color);';
+    } else if(json.background === "darker") {
+      sidebarStyle += '--custom-active-background: rgba(0,0,0,0.2);';
+    } else if(json.background !== "white") {
+      sidebarStyle += `--custom-active-background: ${json.background};`;
+    }
+
+    if (json.icon === "white") {
+      sidebarStyle += '--ic-brand-global-nav-ic-icon-only-svg-fill--active: #FFFFFF;';
+    } else if (json.icon === "black") {
+      sidebarStyle += '--ic-brand-global-nav-ic-icon-only-svg-fill--active: #1d1d21;';
+    } else if (json.icon === "unset") {
+      sidebarStyle += '--ic-brand-global-nav-ic-icon-only-svg-fill--active: var(--ic-brand-global-nav-ic-icon-svg-fill--active);';
+    } else {
+      sidebarStyle += `--ic-brand-global-nav-ic-icon-only-svg-fill--active: ${json.icon};`;
+    }
     document.querySelector('#header').style = sidebarStyle;
   }
 });
