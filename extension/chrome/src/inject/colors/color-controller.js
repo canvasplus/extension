0ace4b7368c464b40a85b830cbd896bac41b45b7
adrWasTaken <67245238+adrWasTaken@users.noreply.github.com>
2021-08-25 21:17:27 -0500

    Make color switching instant

--- extension/chrome/src/inject/colors/color-controller.js
+++ extension/chrome/src/inject/colors/color-controller.js
@@ -1,23 +1,36 @@
-chrome.storage.local.get(['canvasplus-display-appearance'], function(data) {
-  var linkApplyVars = document.createElement("link");
-  linkApplyVars.href = chrome.extension.getURL("src/inject/colors/apply-variables.css");
-  linkApplyVars.type = "text/css";
-  linkApplyVars.rel = "stylesheet";
+const linkApplyVars = document.createElement('link');
+  linkApplyVars.href = '';
+  linkApplyVars.type = 'text/css';
+  linkApplyVars.rel = 'stylesheet';
 
-  var link = document.createElement("link");
-  link.href = chrome.extension.getURL("src/inject/colors/dark.css");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  var link2 = document.createElement("link");
-  link2.href = chrome.extension.getURL("src/inject/colors/dim.css");
-  link2.type = "text/css";
-  link2.rel = "stylesheet";
-    if(data["canvasplus-display-appearance"] === "dark") {
-      document.getElementsByTagName('html')[0].appendChild(link);
-      document.getElementsByTagName('html')[0].appendChild(linkApplyVars);
-    }
-    if(data["canvasplus-display-appearance"] === "dim") {
-      document.getElementsByTagName('html')[0].appendChild(link2);
-      document.getElementsByTagName('html')[0].appendChild(linkApplyVars);
-    }
-});
+const varSheet = document.createElement('link');
+  varSheet.href = '';
+  varSheet.type = 'text/css';
+  varSheet.rel = 'stylesheet';
+
+const html = document.querySelector('html');
+html.appendChild(linkApplyVars);
+html.appendChild(varSheet);
+
+const applyVarsURL = chrome.extension.getURL("src/inject/colors/apply-variables.css");;
+
+const update = (appearance) => {
+  console.log("updating to " + appearance);
+  if(appearance && appearance !== 'light') {
+    linkApplyVars.href = applyVarsURL;
+    varSheet.href = chrome.extension.getURL('src/inject/colors/' + appearance + '.css')
+  } else {
+    linkApplyVars.href = '';
+    varSheet.href = '';
+  }
+}
+
+chrome.storage.local.get(['canvasplus-display-appearance'], (data) => {
+  update(data['canvasplus-display-appearance']);
+})
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if(changes['canvasplus-display-appearance']) {
+    update(changes['canvasplus-display-appearance'].newValue)
+  }
+})
