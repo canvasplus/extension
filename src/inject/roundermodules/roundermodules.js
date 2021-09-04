22443edc79114efd2dc10cd588204f1897f5f9dc
J Coding <jlozier99@gmail.com>
2021-09-04 15:22:57 -0500

    re-organize files

---
+++ src/inject/roundermodules/roundermodules.js
@@ -0,0 +1,12 @@
+const link = document.createElement('link');
+    link.href = chrome.extension.getURL("src/inject/roundermodules/roundermodules.css");;
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+
+chrome.storage.local.get(['canvasplus-setting-roundermodules'], (data) => {
+    if(data['canvasplus-setting-roundermodules']) {
+        if (document.getElementsByClassName("ellipsible")[0].textContent === 'Modules') {
+            ocument.querySelector('html').appendChild(link);
+        }
+    }
+})
