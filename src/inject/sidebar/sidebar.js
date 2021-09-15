add5ae1a6145cddc631687ab0a24ebb4f920522a
adrWasTaken <67245238+adrWasTaken@users.noreply.github.com>
2021-09-14 19:40:22 -0500

    Fix sidebar none settings

--- src/inject/sidebar/sidebar.js
+++ src/inject/sidebar/sidebar.js
@@ -9,7 +9,7 @@ const sidebarIconHeight = addStylingRule('')
 useReactiveFeatures([{
   settingName: 'canvasplus-setting-sidebar-color',
   onChanged: (value) => {
-    sidebarColor.setRule('--sidebar-background-color: ' + value)
+    sidebarColor.setRule('--sidebar-background-color: ' + (value.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/) ? value : 'var(--ic-brand-global-nav-bgd)'))
   }
 }, {
   settingName: 'canvasplus-setting-sidebar-icon-color',
