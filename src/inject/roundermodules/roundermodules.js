904fbda9002324decb7f75211d7c0b7bb7520243
J Coding <jlozier99@gmail.com>
2021-10-15 17:19:03 -0500

    Fix roundermodules partially not working for assignments

--- src/inject/roundermodules/roundermodules.js
+++ src/inject/roundermodules/roundermodules.js
@@ -1,3 +1,12 @@
+const onloadmutation = (el) => new MutationObserver(function(mutations) {
+    mutations.forEach(function() {
+        for (let el of document.querySelectorAll(".element_toggler")) {
+            isclosed(el);
+            mutationObserver(el).observe(el, { attributes: true });
+        }
+    });
+});
+
 const isclosed = (el) => {
     if (el.getAttribute('aria-expanded') == 'true') {
         el.parentElement.parentElement.style.borderRadius = 'var(--modules-open)'
@@ -37,12 +46,7 @@ useReactiveFeatures([{
         if(data) {
             if (Array.from(document.querySelectorAll('span.ellipsible')).find(el => el.textContent === 'Assignments')) {
                 document.documentElement.appendChild(link);
-                window.onload = function() {
-                    for (let el of document.querySelectorAll(".element_toggler")) {
-                        isclosed(el)
-                        mutationObserver(el).observe(el, { attributes: true });
-                    }
-                }
+                onloadmutation(document.getElementById('ag-list')).observe(document.getElementById('ag-list'), { childList: true });
             } else if (Array.from(document.querySelectorAll('span.ellipsible')).find(el => el.textContent == 'Modules')) {
                 document.documentElement.appendChild(link);
                     for (let el2 of document.querySelectorAll('[aria-expanded="true"].ig-header-title.collapse_module_link.ellipsis')) {
