24838f65bec5d8acc8a9a41401b9969ca4cfa692
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-08-13 18:14:46 -0500

    add vertical padding to dialogue box

--- src/index/components/interactive/containers/Dialogue.tsx
+++ src/index/components/interactive/containers/Dialogue.tsx
@@ -2,7 +2,7 @@ import { JSX } from "solid-js";
 
 function Dialogue(props: { children?: JSX.Element }) {
   return (
-    <div className="border border-light-sys-border dark:border-dark-sys-border bg-light-sys-bg dark:bg-dark-sys-bg rounded-md shadow-lg w-32">
+    <div className="border border-light-sys-border dark:border-dark-sys-border bg-light-sys-bg dark:bg-dark-sys-bg rounded-md shadow-lg w-32 py-1">
       {props.children}
     </div>
   );
